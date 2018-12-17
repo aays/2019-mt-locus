194c5b047baf63850edd9db8170886de5301f385
Ahmed <ahmed.hasan@mail.utoronto.ca>
2018-12-16 21:01:34 -0500

    better naming of int files + better progress messages

--- analysis/recombination-ldhelmet/ldhelmet_init.py
+++ analysis/recombination-ldhelmet/ldhelmet_init.py
@@ -37,62 +37,71 @@ def run_ldhelmet(alignment_file, ldhelmet_path, directory, outdir):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
+    coordinates = alignment_file.split('/')[-1].split('.')[0]
+
     find_confs = '{ldhelmet} find_confs \
     --num_threads 10 \
     --window_size 50 \
-    --output_file {directory}temp/output.conf \
+    --output_file {directory}temp/{coordinates}.conf \
     {alignment_file}'.format(
         ldhelmet = ldhelmet_path, 
         directory = directory,
+        coordinates = coordinates,
         alignment_file = alignment_file)
 
     table_gen = '{ldhelmet} table_gen \
     --num_threads 10 \
-    --conf_file {directory}temp/output.conf \
+    --conf_file {directory}temp/{coordinates}.conf \
     --theta 0.01 \
     --rhos 0.0 0.1 10.0 1.0 100.0 \
-    --output_file {directory}temp/output.lk'.format(
+    --output_file {directory}temp/{coordinates}.lk'.format(
         ldhelmet = ldhelmet_path,
+        coordinates = coordinates,
         directory = directory)
 
     pade = '{ldhelmet} pade \
     --num_threads 10 \
-    --conf_file {directory}temp/output.conf \
+    --conf_file {directory}temp/{coordinates}.conf \
     --theta 0.01 \
-    --output_file {directory}temp/output.pade'.format(
+    --output_file {directory}temp/{coordinates}.pade'.format(
         ldhelmet = ldhelmet_path,
+        coordinates = coordinates,
         directory = directory)
 
     rjmcmc = '{ldhelmet} rjmcmc \
     --num_threads 10 \
     --window_size 50 \
     --seq_file {alignment_file} \
-    --lk_file {directory}temp/output.lk \
-    --pade_file {directory}temp/output.pade \
+    --lk_file {directory}temp/{coordinates}.lk \
+    --pade_file {directory}temp/{coordinates}.pade \
     --num_iter 1000000 --burn_in 100000 \
     --block_penalty 50 \
-    --output_file {directory}temp/output.post'.format(
+    --output_file {directory}temp/{coordinates}.post'.format(
         ldhelmet = ldhelmet_path,
         directory = directory,
+        coordinates = coordinates,
         alignment_file = alignment_file)
 
-    coordinates = alignment_file.split('/')[-1].split('.')[0]
     post_to_text = '{ldhelmet} post_to_text \
     --mean \
     --perc 0.025 \
     --perc 0.50 \
     --perc 0.975 \
-    --output_file {outdir}{coordinates}.txt'.format(
+    --output_file {outdir}{coordinates}.txt \
+    {directory}temp/{coordinates}.post'.format(
         ldhelmet = ldhelmet_path,
         outdir = outdir,
-        coordinates = coordinates)
+        coordinates = coordinates,
+        directory = directory)
 
     cmds = [find_confs, table_gen, pade, rjmcmc, post_to_text]
-    print('Starting LDhelmet run for {f}...'.format(f = coordinates))
+    print('LDhelmet run for {f}...'.format(f = coordinates))
     for cmd in cmds:
         child = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                                  stderr = subprocess.PIPE, shell = True)
         stdout, stderr = child.communicate()
+        cmd_name = cmd.split(' ')[1]
+        print('Completed {cmd}.'.format(cmd = cmd_name))
 
     # cleanup
     print('Done.')
