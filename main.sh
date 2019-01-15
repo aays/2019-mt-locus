338a332fd9f5f115ccf67aeb233fba1f43567af0
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2019-01-15 16:55:10 -0500

    update main and plus fasta

--- main.sh
+++ main.sh
@@ -6,7 +6,8 @@
 # this will keep track of times too
 # touch log.txt
 
-# lastz alignment
+### LASTZ ALIGNMENT ###
+
 echo "Aligning mt loci with LASTZ..."
 time ./bin/lastz data/references/mt_plus.fasta data/references/mt_minus.fasta \
 --output=data/alignment-lastz/lastz-align-30k-gapped.maf \
@@ -20,13 +21,15 @@ time ./bin/lastz data/references/mt_plus.fasta data/references/mt_minus.fasta \
 echo "Done."
 
 # clean the bed file
-Rscript analysis/alignment-lastz/clean_lastz_output.R \
--f data/alignment-lastz/lastz-align-30k-gapped.bed \
--o data/alignment-lastz/lastz-align-30k-gapped-filtered.bed
+time python3.5 analysis/alignment-lastz/clean_lastz_output.py \
+--filename data/alignment-lastz/lastz-align-30k-gapped.bed \
+--threshold 0.75 \
+--outfile data/alignment-lastz/lastz-align-30k-gapped-filtered.bed
 
 sleep 3
 
-# aligning the fastas
+### CREATE MT LOCUS FASTAS ###
+
 echo "Creating alignment files..."
 mkdir -p data/aligned-fastas/alignments
 time python3.5 analysis/alignment-lastz/align_mt_fasta_maf.py \
@@ -52,9 +55,26 @@ time python3.5 analysis/alignment-lastz/combine_fastas.py \
 --file data/aligned-fastas/mt_aligned_transposed_filtered.txt \
 --outfile data/aligned-fastas/mt_aligned_final.fasta
 
+echo "Creating C-domain appended file..."
+time ./bin/vcf2fasta.py -v data/references/all_quebec.HC.vcf.gz \
+-r data/references/mtPlus_ref.chromosome_6.fasta \
+-i chromosome_6:826738-943474 \
+-s CC2936 CC2937 CC3060 CC3064 CC3065 CC3068 CC3071 CC3076 CC3086 \
+CC2935 CC2938 CC3059 CC3061 CC3062 CC3063 CC3073 CC3075 CC3079 CC3084 \
+--min_GQ 30 > data/aligned-fastas/c_domain_aligned.fasta
+
+cp data/aligned-fastas/c_domain_aligned.fasta \
+data/aligned-fastas/alignments/chromosome_6_826738-943474.fasta
+
+echo "Appending C domain..."
+time python3.5 analysis/alignment-lastz/append_c_domain.py \
+--mt_aligned data/aligned-fastas/mt_aligned_final.fasta \
+--c_domain data/aligned-fastas/c_domain_aligned.fasta \
+--outfile data/aligned-fastas/mt_aligned_all.fasta
+
 echo "mt-aligned file created."
 echo "Clearing intermediate files..."
-# rm -v data/aligned-fastas/mt_aligned_transposed*
+rm -v data/aligned-fastas/mt_aligned_transposed*
 
 sleep 3
 
@@ -72,7 +92,25 @@ time python3.5 analysis/alignment-lastz/make_mt_only.py \
 --mt_allele minus \
 --output data/aligned-fastas/minus_non_gametolog.fasta
 
+# mask paralogous regions in plus ref
+touch exclusions.intervals
+for line in chromosome_6:535350-542150 chromosome_6:424800-427850; do
+    echo ${line} >> exclusions.intervals;
+done
+
+time python3.5 analysis/alignment-lastz/mask_paralogs.py \
+--filename data/aligned-fastas/plus_strains_ref.fasta \
+--mask_intervals exclusions.intervals \
+--offset 298298 \
+--outfile data/aligned-fastas/plus_strains_ref_masked.fasta
+
+# use masked file from here on out
+mv -v data/aligned-fastas/plus_strains_ref.fasta data/aligned-fastas/plus_strains_ref_unmasked.fasta
+mv -v data/aligned-fastas/plus_strains_ref_masked.fasta data/aligned-fastas/plus_strains_ref.fasta
+
 sleep 3
+
+### RHO ESTIMATION ###
 
 echo "Done."
 echo "Starting recombination rate estimation."
@@ -81,17 +119,17 @@ echo "Starting recombination rate estimation."
 ## gametolog recombination
 echo "LDhelmet runs for aligned fasta..."
 time bash analysis/recombination-ldhelmet/ldhelmet_indiv.sh \
-data/aligned-fastas/mt_aligned_final.fasta
+data/aligned-fastas/mt_aligned_all.fasta
 
 sleep 3
 
-mv -v data/recombination-ldhelmet/recombination-estimates/mt_aligned_final.txt \
+mv -v data/recombination-ldhelmet/recombination-estimates/mt_aligned_all.txt \
 data/recombination-ldhelmet/recombination-estimates/mt_aligned_raw.txt # rename
 
 echo "Correcting coordinates for LDhelmet output..."
 time python3.5 analysis/recombination-ldhelmet/ldhelmet_aligned_clean.py \
 -f data/recombination-ldhelmet/recombination-estimates/mt_aligned_raw.txt \
--o data/recombination-ldhelmet/recombination-estimates/mt_aligned_final.txt
+-o data/recombination-ldhelmet/recombination-estimates/mt_aligned_all.txt
 
 ## non-gametolog recombination
 echo "LDhelmet runs for individual mt loci..."
@@ -120,7 +158,7 @@ sleep 3
 
 echo "Generating long-form recombination estimates..."
 time python3.5 analysis/recombination-ldhelmet/generate_mt_long.py \
---mt_locus data/recombination-ldhelmet/recombination-estimates/mt_aligned_final.txt \
+--mt_locus data/recombination-ldhelmet/recombination-estimates/mt_aligned_all.txt \
 --plus data/recombination-ldhelmet/recombination-estimates/plus_strains_ref_corrected.txt \
 --alignment data/alignment-lastz/lastz-align-30k-gapped-filtered.bed \
 --fasta data/aligned-fastas/plus_strains_ref.fasta \
@@ -130,38 +168,53 @@ echo "Done."
 
 sleep 3
 
+### MT LOCUS-WIDE LD ESTIMATIONS ###
+
 echo "Begin LD calculation across mt locus."
-echo "Transposing aligned fasta...'
-time python3.5 analysis/ld-windowed/transpose_aligned_fasta.py \
---fasta data/aligned-fastas/mt_aligned_final.fasta \
---outfile data/ld-windowed/mt_aligned_long.txt \
---offset 298298 # mt plus coordinates
 
-echo "Done."
+mkdir -p data/ld-windowed/alignments-temp
+mkdir -p data/ld-windowed/r2
+mkdir -p data/ld-windowed/zns
+
+count=1
+filecount=$(ls data/aligned-fastas/alignments/*fasta | wc -l)
+
+# loop over alignments
+for fname in data/aligned-fastas/alignments/*fasta; do
+    base=$(basename $fname .fasta)
+    echo "Currently on ${base}"
+    echo "File ${count} of ${filecount}"
+
+    python3.5 analysis/ld-windowed/transpose_aligned_fasta.py \
+    --fasta ${fname} \
+    --outfile data/ld-windowed/alignments-temp/${base}_long.txt \
+    --infer_offset
+
+    sleep 1
+
+    echo "Calculating r2..."
+    python3.5 analysis/ld-windowed/r2_calc.py \
+    --filename data/ld-windowed/alignments-temp/${base}_long.txt \
+    --windowsize 1000 \
+    --outfile data/ld-windowed/r2/${base}_r2_1k.txt
+
+    sleep 1
+
+    echo "Calculating ZnS..."
+    python3.5 analysis/ld-windowed/zns_calc.py \
+    --filename data/ld-windowed/r2/${base}_r2_1k.txt \
+    --windowsize 1000 \
+    --outfile data/ld-windowed/zns/chromosome_6_zns_1k.txt
+
+    (( count ++ ))
+
+done
+
+rm data/ld-windowed/alignments-temp/*
 
 sleep 3
 
-echo "Calculating pairwise LD in 1 kb windows..."
-time python3.5 analysis/ld-windowed/r2_calc.py \
---filename data/ld-windowed/mt_aligned_long.txt \
---windowsize 1000 \
---outfile data/ld-windowed/mt_aligned_r2_1k.txt
-
-echo "Done."
-
-sleep 3
-
-echo "Computing ZnS statistic in 1 kb windows..."
-time python3.5 analysis/ld-windowed/zns_calc.py \
---filename data/ld-windowed/mt_aligned_r2_1k.txt \
---windowsize 1000 \
---outfile data/ld-windowed/mt_aligned_zns_1k.txt
-echo "ZnS calculation completed."
-
-
-sleep 3
-
-# organelle linkage pipeline
+### ORGANELLE LINKAGE ###
 
 # mtDNA with mt-
 time python3.5 analysis/organelle-linkage/ld_calc.py \
@@ -185,3 +238,4 @@ time python3.5 analysis/organelle-linkage/ld_calc.py \
 time python3.5 analysis/organelle-linkage/all_pairs_calc.py \
 --vcf_file data/organelle_linkage/vcfs/all_variants_filtered.vcf.gz \
 --outfile data/organelle_linkage/allpairs.txt
+
